662266830c2c38b8f77d834c137f1222eb3f3abf
Rodrigo Hiemer <rodrigohiemer@hotmail.com>
2017-05-10 06:00:34 -0300

    inclusão com a linha do modulo do mysql

--- gitCommand.sh
+++ gitCommand.sh
@@ -3,5 +3,6 @@ git init
 git remote add origin -f https://github.com/rhiemer/wildfly-10.1.0.Final.git
 git config core.sparsecheckout true
 echo "standalone/configuration/standalone-full.xml" >> .git/info/sparse-checkout
+echo "modules/system/layers/base/com/mysql/*" >> .git/info/sparse-checkout
 rm standalone/configuration/standalone-full.xml
 git pull origin develop
